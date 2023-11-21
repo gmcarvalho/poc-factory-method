d4b0f09bb0fb233eca0bcd768b33c6653f125430
gabrielacarvalho-hotmart <gabriela.carvalho@hotmart.com>
2023-11-21 13:00:23 -0300

    code review

--- src/main/kotlin/com/kotlintrainning/pocfactorymethod/PocFactoryMethodApplication.kt
+++ src/main/kotlin/com/kotlintrainning/pocfactorymethod/PocFactoryMethodApplication.kt
@@ -1,13 +1,11 @@
 package com.kotlintrainning.pocfactorymethod
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Configuration
 
 
-@Configuration
-@EnableAutoConfiguration(exclude = [DataSourceAutoConfiguration::class])
+@SpringBootApplication
 class PocFactoryMethodApplication
 
 fun main(args: Array<String>) {
