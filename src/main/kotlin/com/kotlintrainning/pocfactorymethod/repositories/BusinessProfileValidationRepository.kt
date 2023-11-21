3b9350da1d96e9dd4ad7c70ca56a7f5eb3bfc706
gabrielacarvalho-hotmart <gabriela.carvalho@hotmart.com>
2023-11-21 11:07:56 -0300

    repository creation

---
+++ src/main/kotlin/com/kotlintrainning/pocfactorymethod/repositories/BusinessProfileValidationRepository.kt
@@ -0,0 +1,8 @@
+package com.kotlintrainning.pocfactorymethod.repositories
+
+import com.kotlintrainning.pocfactorymethod.model.entities.BusinessProfileValidation
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface BusinessProfileValidationRepository: JpaRepository<BusinessProfileValidation, Long>
---
+++ src/main/kotlin/com/kotlintrainning/pocfactorymethod/services/ValidationConfigService.kt
@@ -0,0 +1,8 @@
+package com.kotlintrainning.pocfactorymethod.services
+
+import org.springframework.stereotype.Service
+
+@Service
+class ValidationConfigService {
+
+}
